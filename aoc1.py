4a27da90a9ba0617dba1ae5d501d974c4c6e9273
Thijs Bakker <thijs.bakker@inergy.nl>
2023-04-01 17:23:33 +0200

    day 1 first problem solved

--- aoc1.py
+++ aoc1.py
@@ -3,9 +3,12 @@ from aoc_template import file_to_list
 def main():
     input = file_to_list('aoc1input.txt')
 
+    # for line in input:
+    #     print(line)
     max = 0
     rolling_sum = 0
     for each in input:
+        print(max, rolling_sum, each)
         if each:
             rolling_sum += each
         else:
